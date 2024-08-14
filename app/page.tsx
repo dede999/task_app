2c8dea0c2317649de07210d3cb4b99114f5dc83a
Andre Luiz Abdalla Silveira <andreluizsa00@gmail.com>
2024-08-14 17:06:46 -0300

    chore: Update home page with Entrance component
    The home page has been modified to include the new Entrance component, which handles user entrance logic and provides options for signing up or signing in. This change improves the user experience by centralizing the user entrance functionality.

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import Navbar from "@/components/Navbar";
+import Entrance from "@/components/User/Entrance";
 
 export default function Home() {
   return (
     <main>
       <Navbar />
+      <div className="my-5 text-center">
+        <Entrance />
+      </div>
     </main>
   );
 }
